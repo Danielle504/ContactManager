50884b6e136e6df518b84bffc0a48d48b6108e47
Davis Goff <davispgoff@gmail.com>
2020-01-12 17:37:00 -0500

    Update how nodes are called and add showing ability of editing

--- src/scripts/contacts.js
+++ src/scripts/contacts.js
@@ -1,66 +1,160 @@
-const CONT_HOVER = 1;
-const CONT_INFO = 3;
-const CONT_CTRL = 3
+const CONT_HOVER = 0;
+const CONT_INFO = 1;
+const CONT_CTRL = 1;
 
 document.addEventListener(`DOMContentLoaded`, () => {
+	let editing = false;
+
+	// show the edit and delte buttons
 	const showContactCtrl = event => {
-		if (event.target.childNodes[CONT_HOVER].childNodes[CONT_CTRL] == undefined || !event.target.childNodes[CONT_HOVER].childNodes[CONT_CTRL].classList.contains(`contact-ctrl`)) {
+		const controls = event.target.children[CONT_HOVER].children[CONT_CTRL];
+
+		if (controls == undefined || !controls.classList.contains(`contact-ctrl`)) {
 			return;
 		}
 
-		event.target.childNodes[CONT_HOVER].childNodes[CONT_CTRL].setAttribute(`aria-hidden`, `false`);
+		controls.setAttribute(`aria-hidden`, `false`);
 	};
 
+	// hide the edit and delete buttons
 	const hideContactCtrl = event => {
-		if (event.target.childNodes[CONT_HOVER].childNodes[CONT_CTRL] == undefined || !event.target.childNodes[CONT_HOVER].childNodes[CONT_CTRL].classList.contains(`contact-ctrl`)) {
+		const controls = event.target.children[CONT_HOVER].children[CONT_CTRL];
+
+		if (controls == undefined || !controls.classList.contains(`contact-ctrl`)) {
 			return;
 		}
 
-		event.target.childNodes[CONT_HOVER].childNodes[CONT_CTRL].setAttribute(`aria-hidden`, `true`);
+		controls.setAttribute(`aria-hidden`, `true`);
 	};
 
+	// click on the contact card
+	// will show/hide details, unless editing
 	const contactClick = event => {
-		if (event.target.nodeName === `H3` || event.target.nodeName === `IMG`) {
-			contactClick({
-				target: event.target.parentNode.parentNode.parentNode
-			});
-		}
-
-		if (event.target.classList.contains(`contact-hover-box`)) {
-			contactClick({
-				target: event.target.parentNode
-			});
-		}
-
-		if (event.target.childNodes[CONT_HOVER] == undefined || event.target.childNodes[CONT_HOVER].childNodes[CONT_CTRL] == undefined || !event.target.childNodes[CONT_HOVER].childNodes[CONT_CTRL].classList.contains(`contact-ctrl`)) {
+		if (editing) {
 			return;
 		}
 
-		if (event.target.childNodes[CONT_INFO] == undefined || !event.target.childNodes[CONT_INFO].classList.contains(`contact-info`)) {
+		const target = event.target;
+
+		if (target.nodeName === `BUTTON` || target.parentNode.nodeName === `BUTTON`) {
 			return;
 		}
 
-		if (event.target.childNodes[CONT_INFO].getAttribute(`aria-hidden`) === `true`) {
-			event.target.childNodes[CONT_INFO].setAttribute(`aria-hidden`, `false`);
-			event.target.childNodes[CONT_HOVER].childNodes[CONT_CTRL].setAttribute(`aria-hidden`, `false`);
+		if (target.nodeName === `H3` || target.nodeName === `IMG`) {
+			contactClick({
+				target: target.parentNode.parentNode.parentNode
+			});
+		}
+
+		if (target.classList.contains(`contact-hover-box`)) {
+			contactClick({
+				target: target.parentNode
+			});
+		}
+
+		const hover = target.children[CONT_HOVER];
+
+		if (hover == undefined || hover.children[CONT_CTRL] == undefined || !hover.children[CONT_CTRL].classList.contains(`contact-ctrl`)) {
+			return;
+		}
+
+		const controls = hover.children[CONT_CTRL];
+		const info = target.children[CONT_INFO];
+
+		if (info == undefined || !info.classList.contains(`contact-info`)) {
+			return;
+		}
+
+		if (info.getAttribute(`aria-hidden`) === `true`) {
+			info.setAttribute(`aria-hidden`, `false`);
+			controls.setAttribute(`aria-hidden`, `false`);
 		}
 		else {
-			event.target.childNodes[CONT_INFO].setAttribute(`aria-hidden`, `true`);
+			info.setAttribute(`aria-hidden`, `true`);
 		}
 	};
 
+	// deletes the main contact when the delete icon is pressed
+	// @TODO
+	// add confirmation
+	const deleteClick = event => {
+		event.target.parentNode.parentNode.parentNode.parentNode.remove();
+	};
+
+	// shows contact info if not already up
+	// switches to editing the info
+	const editClick = event => {
+		let info = event.target.parentNode.parentNode.parentNode;
+
+		if (!info.classList.contains(`contact`)) {
+			info = info.parentNode;
+		}
+
+		info = info.children[CONT_INFO];
+
+		if (editing) {
+			if (event.target.children[0] != undefined && event.target.children[0].getAttribute(`src`) === `./img/check.svg`) {
+				event.target.children[0].setAttribute(`src`, `./img/edit-icon.svg`);
+			}
+			else if (event.target.getAttribute(`src`) === `./img/check.svg`) {
+				event.target.setAttribute(`src`, `./img/edit-icon.svg`);
+			}
+
+			editing = false;
+		}
+		else {
+			if (info.getAttribute(`aria-hidden`) === `true`) {
+				info.setAttribute(`aria-hidden`, `false`);
+			}
+
+			if (event.target.children[0] != undefined && event.target.children[0].getAttribute(`src`) === `./img/edit-icon.svg`) {
+				event.target.children[0].setAttribute(`src`, `./img/check.svg`);
+			}
+			else if (event.target.getAttribute(`src`) === `./img/edit-icon.svg`) {
+				event.target.setAttribute(`src`, `./img/check.svg`);
+			}
+
+			editing = true;
+		}
+	};
+
+	// adds in a new contact and allows the info to be edited
+	const addClick = event => {
+		console.log(`clicked add`);
+	};
+
+	// switches color of + when hovering over add button
+	const addHover = event => {
+		if (event.target.children[0].getAttribute(`src`) === `img/plus-icon-light.svg`) {
+			event.target.children[0].setAttribute(`src`, `img/plus-icon-dark.svg`);
+		}
+	};
+
+	// switches color of + when hovering over add button
+	const addLeave = event => {
+		if (event.target.children[0].getAttribute(`src`) === `img/plus-icon-dark.svg`) {
+			event.target.children[0].setAttribute(`src`, `img/plus-icon-light.svg`);
+		}
+	};
+
+	// elements selection
 	const contacts = document.getElementsByClassName(`contact`);
+	const deleteButtons = document.getElementsByClassName(`delete-btn`);
+	const editButtons = document.getElementsByClassName(`edit-btn`);
+	const addButton = document.getElementById(`add-btn`);
+
+	// adding event listeners
+	addButton.onclick = addClick;
+	addButton.onmouseenter = addHover;
+	addButton.onmouseleave = addLeave;
 
 	for (let i=0;i<contacts.length;i++) {
 		contacts.item(i).onmouseenter = showContactCtrl;
 		contacts.item(i).onmouseleave = hideContactCtrl;
 
 		contacts.item(i).onclick = contactClick;
-	}
 
-	const hideAll = () => {
-		for (let i=0;i<contacts.length;i++) {
-			contacts.item(i).childNodes[CONT_HOVER].setAttribute(`aria-hidden`, `true`);
-		}
-	};
+		deleteButtons.item(i).onclick = deleteClick;
+		editButtons.item(i).onclick = editClick;
+	}
 });
