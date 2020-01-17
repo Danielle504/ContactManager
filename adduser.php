bbf91dd90965fac04a09f03091766f24670ae1c3
Ben Iliff <bengiliff@gmail.com>
2020-01-17 13:19:11 -0500

    Add files via upload

---
+++ adduser.php
@@ -0,0 +1,24 @@
+<?php
+  $host = 'https://pregradcrisis.azurewebsites.net/';
+  $user = 'PGC41';
+  $pword = 'P660224chaz0015';
+  $dbase = 'contactdb';
+  $con = mysqli_connect($host, $user, $pword, $dbase);
+
+  if ($con->connect_error)
+  {
+    die("Connection aborted: " . $con->connect_error);
+  }
+
+  $json = file_get_contents('php://input');
+  $obj = json_decode($json);
+
+  $query = "INSERT INTO users(`uid`, `pword`)
+            VALUES ('$obj->uid','$obj->pword')";
+
+  if (!mysqli_query($con, $query))
+  {
+    echo "Error: " . $query . "<br>" . mysqli_error($con);
+  }
+  mysqli_close($con);
+?>
