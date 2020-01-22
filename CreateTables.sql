f4c90406d2a70be4d70fa53922e1c3cb2786a52f
Ben Iliff <bengiliff@gmail.com>
2020-01-22 16:53:23 -0500

    Update CreateTables.sql

--- CreateTables.sql
+++ CreateTables.sql
@@ -1,9 +1,3 @@
-drop database if exists testing;
-
-create database testing;
-
-use testing;
-
 create table users(
 uid varchar(60) not null,
 pword varchar(25) not null,
@@ -15,6 +9,8 @@ fname varchar(60) not null,
 lname varchar(60),
 phone varchar(60) not null,
 email varchar(60),
+cid varchar(60) not null,
+primary key (cid),
 uid varchar(60) not null,
     FOREIGN KEY (uid)
     REFERENCES users (uid)
