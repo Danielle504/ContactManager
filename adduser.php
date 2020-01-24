6517cabd3fd660bcf800bb5ae0e9dd7ee52d43ef
Christian-Young <45320996+Christian-Young@users.noreply.github.com>
2020-01-24 17:05:21 -0500

    Add files via upload

--- adduser.php
+++ adduser.php
@@ -1,12 +1,15 @@
 <?php
   $server = "azcontact.mysql.database.azure.com";
-  $user = "PGC42@azcontact",
+  $user = "PGC42@azcontact";
   $password = "P660224chaz0015";
-  $databse = "azcontact";
+  $database = "contact_manager";
   $con = mysqli_connect($server, $user, $password, $database);
 
   if (!$con)
   {
+    echo "Error: Unable to connect to MySQL." . PHP_EOL;
+    echo "Debugging errno: " . mysqli_connect_errno() . PHP_EOL;
+    echo "Debugging error: " . mysqli_connect_error() . PHP_EOL;
     $obj->code = 500;
     $json = json_encode($obj);
     echo $json;
@@ -16,11 +19,12 @@
   $json = file_get_contents('php://input');
   $obj = json_decode($json);
 
-  $query = "INSERT into 'users'(uid, pword)
-                            VALUES ('$obj->uid, $obj->pword')";
+  $query = "INSERT into users (uid, pword)
+                            VALUES ('$obj->uid', '$obj->pword')";
 
   if (!mysqli_query($con, $query))
   {
+    $obj->error = "Error: " . mysqli_error($con);
     $obj->code = 400;
     $json = json_encode($obj);
     echo $json;
