e2a06e95e409cc5d96be710464ba3beb1261a514
Goff-Davis <davispgoff@gmail.com>
2020-01-26 15:04:56 -0500

    Add log in and register requests

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -1,4 +1,14 @@
+const REGISTER_NAME = `davis@test.com`;
+const REGISTER_PWORD = `testpassword`;
+
+const LOGIN_NAME = `davis@test.com`;
+const LOGIN_PWORD = `testpassword`;
+
+const url = `https://pregradcrisis.azurewebsites.net`;
+
 document.addEventListener(`DOMContentLoaded`, () => {
+	let register = false;
+
 	const inputBlur = event => {
 		if (event.target.value !== ``) {
 			event.target.labels[0].setAttribute(`style`, `visibility: hidden;`);
@@ -13,17 +23,71 @@ document.addEventListener(`DOMContentLoaded`, () => {
 	};
 
 	const handleSend = event => {
+		event.preventDefault();
+
 		if (usernameInput.validity.valid && passwordInput.validity.valid) {
-			console.log(`This is where I'd send my info. If I had any!`);
+			if (register) {
+				const register = new Promise((resolve, reject) => {
+					const request = new XMLHttpRequest();
+					const body = {
+						uid: REGISTER_NAME,
+						pword: REGISTER_PWORD
+					};
+
+					request.open(`POST`, `${url}/adduser.php`);
+					request.setRequestHeader(`Content-type`, `application/json`)
+					request.onload = () => resolve(JSON.parse(request.responseText));
+					request.onerror = () => reject(request.statusText);
+					request.send(JSON.stringify(body));
+				});
+
+				register.then(
+					data => {
+						console.log(data);
+					}
+				).catch(
+					reason => {
+						console.error(reason);
+					}
+				);
+			}
+			else {
+				const login = new Promise((resolve, reject) => {
+					const request = new XMLHttpRequest();
+					const body = {
+						uid: LOGIN_NAME,
+						pword: LOGIN_PWORD
+					};
+
+					request.open(`POST`, `${url}/login.php`);
+					request.setRequestHeader(`Content-type`, `application/json`)
+					request.onload = () => resolve(JSON.parse(request.responseText));
+					request.onerror = () => reject(request.statusText);
+					request.send(JSON.stringify(body));
+				});
+
+				login.then(
+					data => {
+						console.log(data);
+					}
+				).catch(
+					reason => {
+						console.error(reason);
+					}
+				);
+
+			}
 		}
 	};
 
 	const toggleType = event => {
 		if (sendBtn.innerHTML === `Log in`) {
+			register = true;
 			sendBtn.innerHTML = `Register`;
 			toggleBtn.innerHTML = `Or log in`;
 		}
 		else {
+			register = false;
 			sendBtn.innerHTML = `Log in`;
 			toggleBtn.innerHTML = `Or register`;
 		}
